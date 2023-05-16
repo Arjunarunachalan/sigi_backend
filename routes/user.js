d2a7584e94fe4b1242cd5520e4e7c5a3601facc7
Arjun <arju04551@gmail.com>
2023-05-16 15:12:57 +0530

    for pull master

--- routes/user.js
+++ routes/user.js
@@ -25,7 +25,7 @@ async function (req, res, next) {
   const { errors } = validationResult(req);
     if (errors.length>0) {
       return res.status(400).json({ errors}) ;
-    }
+    }git 
 
     const { email, password, phone } = req.body;
     const user = await User.findOne({ email: email });
